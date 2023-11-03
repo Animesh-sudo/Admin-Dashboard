616d20ba5426364966a1e47a5d17eb7014bcc147
Animesh-sudo <animesh2234@gmail.com>
2023-11-03 14:41:33 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -18,13 +18,13 @@ function App() {
       <div className="container">
         <Sidebar/>
         <Routes>
-          <Route path='/' element={ <Home/> }/>
-          <Route path='/users' element={ <UserList/>}/>
-          <Route path='/user/:userId' element={ <User/> }/>
-          <Route path='/newUser' element={ <NewUser/>}/>
-          <Route path='/products' element={ <ProductList/> } />
-          <Route path='/product/:productId' element={ <Product/> }/>
-          <Route path='/newproduct' element={ <NewProduct/>}/>
+          <Route path='/Admin-Dashboard/' element={ <Home/> }/>
+          <Route path='/Admin-Dashboard/users' element={ <UserList/>}/>
+          <Route path='/Admin-Dashboard/user/:userId' element={ <User/> }/>
+          <Route path='/Admin-Dashboard/newUser' element={ <NewUser/>}/>
+          <Route path='/Admin-Dashboard/products' element={ <ProductList/> } />
+          <Route path='/Admin-Dashboard/product/:productId' element={ <Product/> }/>
+          <Route path='/Admin-Dashboard/newproduct' element={ <NewProduct/>}/>
         </Routes>
       </div>
     </div>
